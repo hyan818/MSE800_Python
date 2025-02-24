fbc5177f3bee49fd84ff54f588b2ea3d544b24c7
Yan Huang <hyan818@gmail.com>
2025-02-25 12:13:49 +1300

    refactoring

--- Week12/static/script.js
+++ Week12/static/script.js
@@ -5,13 +5,3 @@ document.getElementById("aboutBtn").addEventListener("click", () => {
 document.getElementById("alertBtn").addEventListener("click", () => {
   alert("Hello from JavaScript!");
 });
-
-document.getElementById("fileInput").addEventListener("change", (event) => {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-
-  reader.onload = function (e) {
-    document.getElementById("preview").src = e.target.result;
-  };
-  reader.readAsText(file);
-});
