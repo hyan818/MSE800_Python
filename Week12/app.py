9655694d77558b7aa0bb6259a1eaf70c797ab6f7
Yan Huang <hyan818@gmail.com>
2025-02-25 11:58:28 +1300

    add more code

--- Week12/app.py
+++ Week12/app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about/<name>/<int:number>")
+def about(name, number):
+    return render_template("about.html", name=name, number=number)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- Week12/static/script.js
+++ Week12/static/script.js
@@ -1,3 +1,17 @@
-document.getElementById('alertBtn').addEventListener('click', function() {
-    alert('Hello from JavaScript!');
+document.getElementById("aboutBtn").addEventListener("click", () => {
+  window.location.href = "/about/Isabel/6";
+});
+
+document.getElementById("alertBtn").addEventListener("click", () => {
+  alert("Hello from JavaScript!");
+});
+
+document.getElementById("fileInput").addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    document.getElementById("preview").src = e.target.result;
+  };
+  reader.readAsText(file);
 });
